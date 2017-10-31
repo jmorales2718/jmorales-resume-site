8cf1d713b74489e393b75ce4b00c89b96429929c
jmorales2718 <john.morales@richmond.edu>
2017-10-31 12:04:41 -0400

    adding partials and ejs

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,3 +1,16 @@
-module.exports.index = function(req, res){
-  res.render('index', {title: 'Express'});
+module.exports.home = function(req, res){
+  res.render('home', {title: 'Home'});
+};
+
+module.exports.about = function(req, res){
+  res.render('about', {title: 'About'});
+};
+
+
+module.exports.portfolio = function(req, res){
+  res.render('portfolio', {title: 'Portfolio'});
+};
+
+module.exports.contact = function(req, res){
+  res.render('contact', {title: 'Contact'});
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 var ctrlMain = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', ctrlMain.index);
+router.get('/', ctrlMain.home);
+router.get('/home', ctrlMain.home);
+router.get('/about', ctrlMain.about);
+router.get('/portfolio', ctrlMain.portfolio);
+router.get('/contact', ctrlMain.contact);
 
 module.exports = router;
